d4f679f6e8ad1158d4e8e98392794e52b5cbb367
charleestefanski <apstef@umich.edu>
2023-01-30 19:55:16 -0500

    adding edge docker and cam

---
+++ edge_vm/cam.py
@@ -0,0 +1,29 @@
+# this is from https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
+import numpy as np
+import cv2
+
+# the index depends on your camera setup and which one is your USB camera.
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('opencv/data/haarcascades/haarcascade_frontalface_default.xml')
+# print(face_cascade.empty())
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    
+    for (x, y, w, h) in faces:
+    	cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
