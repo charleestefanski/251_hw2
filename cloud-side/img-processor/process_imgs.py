d13572886e16d98f653d76d2509c0cf264cb9a05
Ubuntu <ubuntu@ip-172-31-13-8.ec2.internal>
2023-02-08 05:22:15 +0000

    changes to image processor

--- cloud-side/img-processor/process_imgs.py
+++ cloud-side/img-processor/process_imgs.py
@@ -1,9 +1,13 @@
 import paho.mqtt.client as mqtt
 import boto3
+import uuid
 
 LOCAL_MQTT_HOST="mosquitto-service"
 LOCAL_MQTT_PORT=1883
-LOCAL_MQTT_TOPIC="faces_remote"
+LOCAL_MQTT_TOPIC="faces"
+
+s3 = boto3.client('s3')
+bucket_name = 'hw2bucket-cs'
 
 def on_connect_local(client, userdata, flags, rc):
     print("connected to local broker with rc: " + str(rc))
@@ -12,7 +16,10 @@ def on_connect_local(client, userdata, flags, rc):
 def on_message(client,userdata, msg):
     print("recieved a message")
     try:
-        print("message received: ",str(msg.payload.decode("utf-8")))
+        img = msg.payload
+        #s3.upload_file(img, bucket_name)
+        s3.put_object(Bucket=bucket_name, Body=img,Key=str(uuid.uuid4()),ACL='public-read',ContentType='image/png')
+
     # if we wanted to re-publish this message, something like this should work
     # msg = msg.payload
     # remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
